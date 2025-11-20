21dc69ab3737fd5d5396a5be31bbbf017c8bdc76
GooDHous <165495399+GooDHous@users.noreply.github.com>
2025-11-20 21:10:11 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+new Snow ({			
+        showSnowBalls: false,
+        icon:'<div style="border-radius:50%; display:flex; width:16px; height:16px; background-color:white;"> <img src="https://ywteam.ru/img/sf.ico" height="16px" width"16px"" alt="">',
+		showSnowBallsIsMobile: false,
+		showSnowflakes: true,
+		countSnowflake: 40,
+		snowBallsLength: 50,
+		snowBallIterations: 1000,
+		snowBallupNum: 2,
+		snowBallIterationsInterval: 10,
+		clearSnowBalls: 20000,
+});
+
+function copyCommand() {
+    const command = "irm get.ywteam.ru | iex";
+    navigator.clipboard.writeText(command).then(() => {
+        alert("Команда скопирована в буфер обмена!");
+    });
+}
+
+function copyUninstallCommand() {
+    const command = "irm get.ywteam.ru/uninstall | iex";
+    navigator.clipboard.writeText(command).then(() => {
+        alert("Команда скопирована в буфер обмена!");
+    });
+}
+
+function redirect(url) {
+    window.open(url, '_blank');
+}
+
+window.addEventListener('scroll', function() {
+    const backToTop = document.querySelector('.back-to-top');
+    if (window.pageYOffset > 300) {
+        backToTop.classList.add('visible');
+    } else {
+        backToTop.classList.remove('visible');
+    }
+});
+
+document.querySelector('.back-to-top').addEventListener('click', function(e) {
+    e.preventDefault();
+    window.scrollTo({top: 0, behavior: 'smooth'});
+});
+
+window.addEventListener('scroll', function() {
+    const header = document.querySelector('header');
+    if (window.pageYOffset > 50) {
+        header.classList.add('scrolled');
+    } else {
+        header.classList.remove('scrolled');
+    }
+});
